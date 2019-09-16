c31695528ad9b7d8b6760cc4c368e2f87c404f0a
HumnaRasheed <31679490+HumnaRasheed@users.noreply.github.com>
2019-09-15 22:47:52 -0500

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>This is HUMNA RASHEED's Assignment 1, To_Do List in vue.js to learn about Vue.js </p>
+  </div>
+</template>
